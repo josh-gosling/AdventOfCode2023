ea10de5e663604501ffb13d731b462ab54a5232d
joshua.gosling <jgosling195@gmail.com>
2023-12-02 20:21:11 +0000

    Day 2 Part 2 complete

--- src/main/kotlin/day2/Main.kt
+++ src/main/kotlin/day2/Main.kt
@@ -3,41 +3,76 @@ package day2
 
 import java.io.File
 
-fun solvePart1(input: List<String>): Int {
+fun solvePart2(subsets: List<String>): Int {
+    var greenMin = 0
+    var redMin = 0
+    var blueMin = 0
+    subsets.forEach { subset ->
+        val cubesRevealed = subset.split(", ")
+        cubesRevealed.forEach { cube ->
+            val quantity = cube.split(" ")[0].toInt()
+            val colour = cube.split(" ")[1]
+
+            when (colour) {
+                "blue" -> if (quantity > blueMin) {
+                    blueMin = quantity
+                }
+
+                "red" -> if (quantity > redMin) {
+                    redMin = quantity
+                }
+
+                "green" -> if (quantity > greenMin) {
+                    greenMin = quantity
+                }
+            }
+        }
+    }
+    return greenMin * blueMin * redMin
+}
+
+fun solvePart1(id: String, subsets: List<String>): MutableList<String> {
     val possibleGames = mutableListOf<String>()
+    var isPossible = true
+    subsets.forEach { subset ->
+        var greenCount = 0
+        var redCount = 0
+        var blueCount = 0
+        val cubesRevealed = subset.split(", ")
+        cubesRevealed.forEach { cube ->
+            val quantity = cube.split(" ")[0].toInt()
+            val colour = cube.split(" ")[1]
+
+            when (colour) {
+                "blue" -> blueCount += quantity
+                "red" -> redCount += quantity
+                "green" -> greenCount += quantity
+            }
+        }
+        if (!(redCount <= 12 && greenCount <= 13 && blueCount <= 14)) {
+            isPossible = false
+        }
+    }
+    if (isPossible) {
+        possibleGames.add(id)
+    }
+    return possibleGames
+}
+
+fun loop(input: List<String>): List<Int> {
+    val possibleGames = mutableListOf<String>()
+    val minNumber = mutableListOf<Int>()
     input.forEach { game ->
         val subsets = game.split(": ")[1].split("; ")
         val id = game.split(": ")[0]
-        var isPossible = true
 
-        subsets.forEach { subset ->
-            var greenCount = 0
-            var redCount = 0
-            var blueCount = 0
-            val cubesRevealed = subset.split(", ")
-            cubesRevealed.forEach { cube ->
-                val quantity = cube.split(" ")[0].toInt()
-                val colour = cube.split(" ")[1]
-
-                when (colour) {
-                    "blue" -> blueCount += quantity
-                    "red" -> redCount += quantity
-                    "green" -> greenCount += quantity
-                }
-            }
-            if (!(redCount <= 12 && greenCount <= 13 && blueCount <= 14)) {
-                isPossible = false
-            }
-        }
-        if (isPossible) {
-            possibleGames.add(id)
-        }
+        possibleGames += solvePart1(id, subsets)
+        minNumber += solvePart2(subsets)
     }
-    println(possibleGames)
-    return possibleGames.map { it.split("Game ")[1].toInt() }.sumOf { it }
+    return listOf(possibleGames.map { it.split("Game ")[1].toInt() }.sumOf { it }, minNumber.sumOf { it })
 }
 
 fun main() {
     val input = File("inputs/day2.txt").readText().replace("\r", "").split("\n")
-    println(solvePart1(input))
+    println(loop(input))
 }
